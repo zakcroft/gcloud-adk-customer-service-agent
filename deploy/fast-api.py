155f8a7c0618cd72f7a8274b7e0ec3d414b0607b
Zak Croft <zak.croft@nagarro.com>
2025-09-29 21:14:53 +0100

    fast api

---
+++ deploy/fast-api.py
@@ -0,0 +1,27 @@
+
+import os
+from google.adk.cli.fast_api import get_fast_api_app
+from fastapi import FastAPI
+
+from agent.config import Config
+
+configs = Config()
+
+DEPLOY_DIR = os.path.dirname(os.path.abspath(__file__))
+AGENT_DIR = os.path.join(os.path.dirname(DEPLOY_DIR), "agent")
+
+print('AGENT_DIR===', AGENT_DIR)
+
+app: FastAPI = get_fast_api_app(
+    agents_dir=AGENT_DIR,
+    web=True,
+    trace_to_cloud=True,
+)
+
+app.title = "customer-services-agent"
+app.description = "API for interacting with the customer services Agent."
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8080)
